49e8e899a12c6d725b5514f7723e354bd5b8f5da
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2022-06-16 18:55:54 +0100

    ..

--- developement/front-end/src/components/Panier.vue
+++ developement/front-end/src/components/Panier.vue
@@ -1,5 +1,6 @@
 <script setup>
 import NavBar from '../components/NavBar.vue'
+import ProductCard from './ProductCard.vue';
 import { onMounted, ref } from 'vue';
 import axios from 'axios';
 import { useStore } from '@/stores/counter'
@@ -10,27 +11,26 @@ const id = store.user.user_id;
 const e = store.count;
 
 
-const aa = ref(0);
+// const aa = ref(0);
 
-const Box = ref([]);
+const items = ref([]);
 
-
-const incriment = function () {
-  aa.value++;
-}
-
-const decriment = function () {
-  while (aa.value > 0) {
-    aa.value--;
-  }
-}
+// const updateQuantiti = function (id) {
+//    const res = axios.post('http://localhost/api/UpdateBasket', id);
+//     const data = res.data;
+//     if(data) {
+//       console.log(data);
+//     } else {
+//       console.log('error');
+//     }
+// }
 
 const GetBasket = async function () {
   const res = await axios.get('http://localhost/api/GetBasket?id=' + id);
   const data = await res.data;
   if (data) {
     e(data);
-    Box.value = data;
+    items.value = data;
   } else {
     console.log("error");
   }
@@ -61,35 +61,7 @@ onMounted (() => {
       <div class="title">
         <h1>Panier</h1>
       </div>
-      <div v-for="box in Box" class="product">
-        <div class="product_img">
-          <img :src="`http://localhost/uploads/` + box.image" alt="" />
-        </div>
-        <div class="product_info">
-          <div class="name">
-            <p>{{box.name}}</p>
-            <p>{{box.description}}</p>
-          </div>
-          <div class="counter">
-            <div>
-              <label>Quantité</label>
-            </div>
-            <div class="count">
-              <button @click="decriment">-</button>
-              <p>{{ aa }}</p>
-              <button @click="incriment">+</button>
-            </div>
-          </div>
-          <div class="price">
-            <label>Prix</label>
-            <p>{{box.price }}</p>
-            <h1>{{ total }}</h1>
-          </div>
-        </div>
-          <div @click="DeletePanier(box.product_id)" class="close">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
-          </div>
-      </div>
+      <ProductCard v-for="item in items" :item="item"/>
       <div class="total">
         <div class="text">
           <label>SOUS-TOTAL</label>
@@ -151,160 +123,6 @@ onMounted (() => {
       font-size: 1.5rem;
      }
   }
-
-  .product {
-    color: black;
-    background-color: white;
-    padding: 0.5rem;
-    display: flex;
-    align-items: center;
-    gap: 3rem;
-    box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.5);
-    border-radius: 0.5rem;
-    margin: 1rem 0;
-
-    .product_img {
-      width: 20%;
-
-      @include mobile {
-        width: 30%;
-      }
-      @include tablet {
-        width: 20%;
-      }
-
-      @include desktop {
-        width: 15%;
-      }
-
-      // @include lg-desktop {
-      //   width: 25%;
-      // }
-      img {
-        border-radius: 0.2rem;
-        width: 100%;
-      }
-    }
-    label {
-      font-size: 16px;
-      font-weight: bold;
-      display: none;
-
-      @include desktop {
-        display: block;
-      }
-
-      @include tablet {
-        display: block;
-      }
-    }
-
-    .product_info {
-      display: flex;
-      flex-direction: column;
-      justify-content: Space-between;
-      width: 50%;
-      gap: 1rem;
-
-      p {
-        font-size: 14px;
-        margin: auto;
-
-        @include tablet {
-          font-size: 16px;
-        }
-        @include desktop {
-          margin-bottom: auto;
-          font-size: 16px;
-
-        }
-      }
-      @include tablet {
-        width: 75%;
-        flex-direction: row;
-        gap: 4rem;
-      }
-      @include desktop {
-        display: flex;
-        flex-direction: row;
-        gap: 6rem;
-      }
-    }
-
-    .counter {
-      margin-right: auto;
-      display: flex;
-      flex-direction: column;
-      
-      @include tablet {
-        display: flex;
-        gap: 2.5rem;
-        text-align: center;
-        font-size: 14px;
-        border-radius: 0.3rem;
-        margin: auto;
-      }
-       @include desktop {
-        gap: 2.5rem;
-        text-align: center;
-        font-size: 14px;
-        border-radius: 0.3rem;
-        margin: auto;
-       }
-      .count {
-        display: flex;
-        gap: 0.6rem;
-      }
-      button {
-        text-align: center;
-        align-items: center;
-        width: 25px;
-        height: 25px;
-        border: none;
-        // background-color: $button_color;
-        font-weight: bold;
-        font-size: 24px;
-        border-radius: 0.3rem;
-
-        @include desktop {
-          width: 2rem;
-          height: 2rem;
-        }
-      }
-
-    }
-
-    .close {
-      margin-left: auto;
-      margin-bottom: auto;
-
-      svg {
-        width: 1.4rem;
-        height: 1.4rem;
-        cursor: pointer;
-      }
-    }
-
-    .price {
-      // padding: 1rem 0;
-      margin-right: auto;
-      display: flex;
-      flex-direction: column;
-      gap: 2.5rem;
-      text-align: center;
-      font-size: 14px;
-      border-radius: 0.3rem;
-
-      @include tablet {
-        margin: auto;
-      }
-       @include desktop {
-        margin: auto;
-       }
-    }
-
-  }
-}
 .total {
   width: 100%;
   margin: 4rem 0;
@@ -339,5 +157,6 @@ onMounted (() => {
       color: white;
     }
   }
+}
 }
 </style>
---
+++ developement/front-end/src/components/ProductCard.vue
@@ -0,0 +1,241 @@
+<script setup>
+const props = defineProps({
+  item: Object,
+});
+const increment = () => props.item.Quantite++;
+const decrement = () => props.item.Quantite--;
+</script>
+<template>
+  <div class="product">
+    <div class="product_img">
+      <img :src="`http://localhost/uploads/` + item.image" alt="" />
+    </div>
+    <div class="product_item">
+        <div class="product_info">
+            <div class="name">
+                <p>{{ item.name }}</p>
+                <p>{{ item.description }}</p>
+            </div>
+            <div class="counter">
+                <div>
+                <label>Quantité</label>
+                </div>
+                <div class="count">
+                <button @click="decrement()">-</button>
+                <p>{{ item.Quantite }}</p>
+                <button @click="increment()">+</button>
+                </div>
+            </div>
+            <div class="price">
+                <label>Prix</label>
+                <p>{{ item.price }}</p>
+            </div>
+        </div>
+        <div class="total">
+            <h3>Prix Total</h3>
+            <p>{{ item.price * item.Quantite }}</p>
+        </div>
+    </div>
+    <div @click="DeletePanier(item.product_id)" class="close">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+        <path
+          d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import "../assets/Scss/variable";
+@import "../assets/Scss/media";
+  .product {
+    color: black;
+    background-color: white;
+    // padding: 0.5rem;
+    display: flex;
+    align-items: center;
+    gap: 3rem;
+    box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.5);
+    border-radius: 0.5rem;
+    margin: 1rem 0;
+
+    .product_img {
+      width: 25%;
+
+      @include mobile {
+        width: 35%;
+      }
+      @include tablet {
+        width: 20%;
+      }
+
+      @include desktop {
+        width: 20%;
+      }
+      img {
+        border-radius: 0.2rem;
+        width: 100%;
+      }
+    }
+    label {
+      font-size: 16px;
+      font-weight: bold;
+      display: none;
+
+      @include desktop {
+        display: block;
+      }
+
+      @include tablet {
+        display: block;
+      }
+    }
+
+    .product_item {
+        margin: auto;
+        display: flex;
+        width: 60%;
+
+
+        @include tablet {
+            margin-top: 1rem;
+            gap: 2rem;
+            display: flex;
+            flex-direction: column;
+        }
+
+        @include desktop {
+            margin-top: 1rem;
+            gap: 5.7rem;
+            display: flex;
+            flex-direction: column;
+        }
+    }
+
+    .total {
+        margin-top: auto;
+        height: 2.5rem;
+        background-color: #e9ecef;
+        align-items: center;
+        text-align: center;
+        display: none;
+
+        @include tablet {
+            display: block;
+            display: flex;
+            justify-content: space-around;
+        }
+        @include desktop {
+            display: block;
+            display: flex;
+            justify-content: space-around;
+
+        }
+
+        h3 {
+            font-size: 16px;
+            font-weight: bold;
+        }
+        p {
+            font-size: 14px;
+        }
+    }
+
+    .product_info {
+
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+
+      p {
+        font-size: 14px;
+      }
+      @include tablet {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        // gap: 5rem;
+      }
+      @include desktop {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+      }
+    }
+
+    .counter {
+    //   margin-right: auto;
+      display: flex;
+      flex-direction: column;
+
+      @include tablet {
+        display: flex;
+        gap: 1rem;
+        text-align: center;
+        font-size: 14px;
+        border-radius: 0.3rem;
+        // margin: auto;
+      }
+      @include desktop {
+        gap: 1rem;
+        text-align: center;
+        font-size: 14px;
+        border-radius: 0.3rem;
+        // margin: auto;
+      }
+      .count {
+        align-items: center;
+        display: flex;
+        gap: 0.6rem;
+      }
+      button {
+        text-align: center;
+        align-items: center;
+        width: 22px;
+        height: 22px;
+        border: none;
+        font-weight: bold;
+        border-radius: 0.3rem;
+
+        @include desktop {
+            font-size: 22px;
+            width: 30px;
+            height: 30px;
+        }
+      }
+    }
+
+    .close {
+        padding: 5px;
+        
+      margin-left: auto;
+      margin-bottom: auto;
+
+      svg {
+        width: 1.4rem;
+        height: 1.4rem;
+        cursor: pointer;
+      }
+    }
+
+    .price {
+    //   margin-right: auto;
+      text-align: center;
+      font-size: 14px;
+      border-radius: 0.3rem;
+      display: flex;
+
+      @include tablet {
+        display: flex;
+          flex-direction: column;
+          gap: 1rem;
+      }
+
+      @include desktop {
+        display: flex;
+          flex-direction: column;
+          gap: 1rem;
+      }
+    }
+  }
+</style>
